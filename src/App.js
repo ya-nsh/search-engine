6d2dc62021bf46ca2814aabca07409d0961be8c0
ya-nsh <yansh2017@gmail.com>
2022-06-12 15:55:16 +0530

    added navbar, footer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import Footer from './components/Footer';
+import Navbar from './components/Navbar';
+import { useState } from 'react';
+import Routes from './routes/Routes';
+// import { Route, Routes } from 'react-router-dom';
 
 function App() {
+  const [darkTheme, setDarkTheme] = useState(false);
+
+  const darkModeHandler = () => {
+    setDarkTheme(darkTheme => !darkTheme);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={darkTheme ? 'dark' : ''}>
+      <div className="bg-gray-300 dark:bg-slate-800 dark:text-gray-200 min-h-screen flex flex-col justify-between">
+        <Navbar darkModeHandler={darkModeHandler} darkTheme={darkTheme} />
+        <Routes />
+        <Footer darkTheme={darkTheme} />
+      </div>
     </div>
   );
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,85 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { MdDarkMode, MdLightMode, MdOutlineDarkMode } from 'react-icons/md';
+
+function Navbar({ darkTheme, darkModeHandler }) {
+  return (
+    <div>
+      <header className={darkTheme ? 'bg-black' : 'bg-white'}>
+        <div className="max-w-screen-xl px-4 mx-auto sm:px-6 lg:px-8">
+          <div className="flex items-center justify-between h-16">
+            <div className="md:flex md:items-center md:gap-12">
+              <Link
+                className={`${darkTheme ? 'text-white' : 'text-black'} block `}
+                to="/"
+              >
+                <span className="font-bold text-xl tracking-tight">
+                  Search Engine 👋
+                </span>
+              </Link>
+            </div>
+
+            <div className="block">
+              <nav aria-labelledby="header-navigation">
+                <ul className="flex items-center gap-6 text-sm">
+                  <li>
+                    <Link
+                      className={`${
+                        darkTheme ? 'text-white' : 'text-black'
+                      }  text-xs transition hover:text-gray-500/75 `}
+                      to="/about"
+                    >
+                      ABOUT
+                    </Link>
+                  </li>
+                </ul>
+              </nav>
+            </div>
+
+            <div className="flex items-center gap-4">
+              <div className="sm:gap-4 sm:flex" onClick={darkModeHandler}>
+                {darkTheme ? (
+                  <div className="flex items-center gap-4">
+                    <div className="inline-block px-8 py-3 text-sm font-medium text-white transition bg-indigo-600 rounded-full hover:scale-110 hover:shadow-xl active:bg-indigo-500 focus:outline-none focus:ring cursor-pointer">
+                      <MdLightMode />
+                    </div>
+                  </div>
+                ) : (
+                  <div className="flex items-center gap-4">
+                    <div className="inline-block px-8 py-3 text-sm font-medium text-indigo-600 transition border border-current rounded-full hover:scale-110 hover:shadow-xl active:text-indigo-500 focus:outline-none focus:ring cursor-pointer">
+                      <MdDarkMode />
+                    </div>
+                  </div>
+                )}
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default Navbar;
+
+// {
+//   darkTheme ? (
+//     <div className="flex items-center gap-4">
+//       <a
+//         className="inline-block px-8 py-3 text-sm font-medium text-white transition bg-indigo-600 rounded hover:scale-110 hover:shadow-xl active:bg-indigo-500 focus:outline-none focus:ring"
+//         href="/"
+//       >
+//         <MdDarkMode />
+//       </a>
+//     </div>
+//   ) : (
+//     <div className="flex items-center gap-4">
+//       <a
+//         className="inline-block px-8 py-3 text-sm font-medium text-indigo-600 transition border border-current rounded hover:scale-110 hover:shadow-xl active:text-indigo-500 focus:outline-none focus:ring"
+//         href="/download"
+//       >
+//         <MdOutlineDarkMode />
+//       </a>
+//     </div>
+//   );
+// }
