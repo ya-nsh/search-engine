9a08f0ac8f47999f4b5d4124f3f1232b00e89f05
ya-nsh <yansh2017@gmail.com>
2022-06-12 17:18:11 +0530

    added framer motion and initialized context

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import logo from './logo.svg';
 import './App.css';
 import Footer from './components/Footer';
 import Navbar from './components/Navbar';
+import Results from './components/Results';
 import { useState } from 'react';
-import Routes from './routes/Routes';
-// import { Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Search from './components/Search';
 
 function App() {
@@ -18,8 +18,16 @@ function App() {
     <div className={darkTheme ? 'dark' : ''}>
       <div className="bg-white dark:bg-slate-800 dark:text-gray-200 min-h-screen flex flex-col justify-between">
         <Navbar darkModeHandler={darkModeHandler} darkTheme={darkTheme} />
-        <Search darkTheme={darkTheme} />
-        <Routes />
+        <Routes>
+          <Route path="/" element={<Search darkTheme={darkTheme} />} />
+
+          <Route path="/search" element={<Results />} />
+          <Route path="/images" element={<Results />} />
+          <Route path="/news" element={<Results />} />
+          <Route path="/videos" element={<Results />} />
+          <Route path="/about" element={<h1>About</h1>} />
+        </Routes>
+
         <Footer darkTheme={darkTheme} />
       </div>
     </div>
---
+++ src/contexts/ResultContextProvider.js
@@ -0,0 +1,28 @@
+import React, { createContext, useContext, useState } from 'react';
+
+const ResultContext = createContext();
+const baseUrl = 'https://google-search3.p.rapidapi.com/api/v1';
+
+export const ResultContextProvider = ({ children }) => {
+  const [results, setResults] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const getResults = async type => {
+    setLoading(true);
+
+    const response = await fetch(`baseUrl${type}`, {
+      method: 'GET',
+      headers: {
+        'X-User-Agent': 'desktop',
+        'X-Proxy-Location': 'EU',
+        'X-RapidAPI-Key': process.env.REACT_APP_RAPIDAPI_KEY,
+        'X-RapidAPI-Host': process.env.REACT_APP_RAPIDAPI_HOST
+      }
+    });
+
+    const data = await response.json();
+    setResults(data);
+    setLoading(false);
+  };
+};
